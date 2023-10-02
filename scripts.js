c012337e97b654df08191445173034741f4719f3
alxxwu <alxxwu@gmail.com>
2023-10-02 13:06:24 +0800

    riot

--- scripts.js
+++ scripts.js
@@ -2,8 +2,9 @@ const checkapi = async () => {
   document.getElementById("riot").innerHTML = "checking...";
   try {
     const response = await fetch(
-      "https://oc1.api.riotgames.com/lol/champion-mastery/v4/scores/by-summoner/2ENqPUNY_TPniBmhqioOcoqFnB3GF22i8PGoH1gf-3yNQw?api_key=RGAPI-75b67a8e-6e4b-44eb-8524-732d7275d624"
+      "https://oc1.api.riotgames.com/lol/champion-mastery/v4/scores/by-summoner/2ENqPUNY_TPniBmhqioOcoqFnB3GF22i8PGoH1gf-3yNQw?api_key=RGAPI-ae97e38b-300a-429f-8ac0-c44ff8136371"
     );
+    // don't do this in production probably, exposed api key
     document.getElementById("riot").innerHTML =
       "wait it's actually up for once";
     document.getElementById("api button").innerHTML = "wow that's crazy";
